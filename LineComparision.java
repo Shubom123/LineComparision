edb87374d3657a702da333e5357d5d0bd1fef56c
Shubom123 <shubomnath365@gmail.com>
2022-06-19 15:10:53 +0530

    Added: Length calculation functionality added

--- LineComparision.java
+++ LineComparision.java
@@ -1,5 +1,12 @@
 public class LineComparision{
 public static void main(String args[]){
+int x1=5;
+int y1=4;
+int x2=7;
+int y2=8;
+double length;
 System.out.println("Welcome to line comparision");
+length = Math.sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2);
+System.out.println("lenght of first line: " + length);
 }
 }
